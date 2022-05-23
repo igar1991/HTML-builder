5997fdba0667893f1c682a66b3b06fa00f03c934
igar1991 <iiggaarr19912004@gmail.com>
2022-05-23 15:50:30 +0300

    feat: add task 3

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const fs = require('fs/promises');
 
+async function copyFile(currentPath, copyPath) {
+  const files = await fs.readdir(currentPath, { withFileTypes: true });
+  files.forEach(el=>{
+    if(!el.isFile()) {
+      fs.mkdir(copyPath+'/'+el.name, {recursive: true}).then(()=>{
+        copyFile(currentPath+'/'+el.name, copyPath+'/'+el.name);
+      });
+      
+    } else {
+      fs.copyFile(currentPath+'/'+el.name,copyPath+'/'+el.name );
+    }
+  });
+}
+
+async function clearFile(path) {
+  const files = await fs.readdir(path, { withFileTypes: true });
+
+  files.forEach(el=>{
+    if(!el.isFile()) {
+      clearFile(path+'/'+el.name).then(()=>{
+        fs.rmdir(path+'/'+el.name, {}, err => {
+          if(err) throw err;
+        });
+      });
+    } else {
+      fs.unlink(path+'/'+el.name);
+    }
+  });
+}
+(async()=>{
+  const folderCopy = await fs.mkdir(__dirname+'/files-copy', {recursive: true});
+  if(folderCopy === undefined) {
+    await clearFile(__dirname+'/files-copy');
+  }
+  copyFile(__dirname+'/files', __dirname+'/files-copy');
+})();
